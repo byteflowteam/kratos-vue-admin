6eed006ffee7f08ff16c5ebe2affd7164f3eb709
dahe <dahe@gmail.com>
2023-09-15 17:06:50 +0800

    update: 用户状态常量值设置

--- pkg/common/constant/constant.go
+++ pkg/common/constant/constant.go
@@ -6,4 +6,10 @@ const (
 
 	// StatusMenusForbidden 表示菜单被禁止
 	StatusMenusForbidden = 2
+
+	// StatusUserNormal 表示账号正常状态
+	StatusUserNormal = 1
+
+	// StatusUserForbidden 表示账号处理停用状态
+	StatusUserForbidden = 2
 )
