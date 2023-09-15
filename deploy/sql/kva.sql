4df85a67fcc8979fe569690a87d8eba767c4c2b7
dahe <dahe@gmail.com>
2023-09-15 14:44:09 +0800

    fix: 初始sql语句添加设置默认时间类型报错

--- deploy/sql/kva.sql
+++ deploy/sql/kva.sql
@@ -16,6 +16,9 @@
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+-- Solve problem insert default datetime error incorrect datetime value: '0000-00-00 00:00:00'
+-- 解决orm插入datetime类型默认值'0000-00-00 00:00:00'会报错问题
+SET @@global.sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- ----------------------------
 -- Table structure for casbin_rule
